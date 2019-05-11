4df3dbf3374afd3f1bf9b3fd75c1a34beb2b39c9
WeversonLuiz <weverson408@gmail.com>
2019-05-11 13:40:18 -0300

    Corrigindo o problema do Height do botao

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _HomeState extends State<Home> {
     );
 
     Container containerBtn = Container(
-      height: 50,
+      height: 200,
       child: raisedButton,
     );
 
@@ -67,10 +67,14 @@ class _HomeState extends State<Home> {
         ],
     );
 
+    SingleChildScrollView singleChildScrollView = SingleChildScrollView(
+      child: column,
+    );
+
     Scaffold scaffold = Scaffold(
       appBar: appBar,
       backgroundColor: Colors.white,
-      body: column,
+      body: singleChildScrollView,
     );
 
     return scaffold;
